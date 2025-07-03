153330c18975dc5aa5ea8659b6ca7b38778206a3
prate <pratesicristian@gmail.com>
2025-07-03 19:24:58 +0200

    Created Django Project. Create app Events e Accounts. Added CustomUser

---
+++ ems/accounts/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    # Aggiungi campi personalizzati se vuoi
+    birth_date = models.DateField(null=True, blank=True)
+    organization = models.CharField(max_length=120, blank=True)
+
+    def __str__(self):
+        return self.username
