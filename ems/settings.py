62fef150dea0ee1e42aaa4a5d0c687264e208802
prate <pratesicristian@gmail.com>
2025-07-10 16:43:15 +0200

    Updated requirements and settings

--- ems/settings.py
+++ ems/settings.py
@@ -17,23 +17,24 @@ from dotenv import load_dotenv
 load_dotenv()
 import environ
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
-
 # SECURITY WARNING: don't run with debug turned on in production!
-
 env = environ.Env(
     DEBUG=(bool, False)
 )
 
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ems.settings')
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("SECRET_KEY")
 
 DEBUG = env("DEBUG", default=False)
 
