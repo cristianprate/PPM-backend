421ef6b7fdd570467256d3064f93357e74d935f9
prate <pratesicristian@gmail.com>
2025-07-10 12:20:09 +0200

    FEAT: added db logic for deployement

--- ems/ems/settings.py
+++ ems/ems/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,11 +90,14 @@ WSGI_APPLICATION = 'ems.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',  # o quello che hai visto su Railway
+        'USER': 'postgres',
+        'PASSWORD': 'CfqJZTYQSWKtYsimmOVVUgdopZHzZWoe',
+        'HOST': 'shortline.proxy.rlwy.net',
+        'PORT': '24279',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
